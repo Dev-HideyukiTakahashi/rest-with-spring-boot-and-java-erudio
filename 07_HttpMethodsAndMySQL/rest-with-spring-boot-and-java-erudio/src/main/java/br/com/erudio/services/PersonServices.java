681f80ec7cdc1a522db32cb48346df3e0e1290f4
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-11-02 04:18:16 -0300

    HttpMethodsAndMySQL

---
+++ 07_HttpMethodsAndMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,68 @@
+package br.com.erudio.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+
+	// Mockar incremento de id
+	// private static final AtomicLong COUNTER = new AtomicLong();
+
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+	@Autowired
+	private PersonRepository repository;
+
+	public Person findById(Long id) {
+
+		logger.info("Finding one person!");
+
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	}
+
+	public List<Person> findAll() {
+
+		return repository.findAll();
+	}
+
+	public Person create(Person person) {
+
+		logger.info("Creating one person!");
+		return repository.save(person);
+	}
+
+	public Person update(Person person) {
+
+		logger.info("Updating one person!");
+
+		Person entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+
+		return repository.save(entity);
+	}
+
+	public void delete(Long id) {
+
+		logger.info("Deleting one person!");
+
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		repository.delete(entity);
+	}
+
+}
