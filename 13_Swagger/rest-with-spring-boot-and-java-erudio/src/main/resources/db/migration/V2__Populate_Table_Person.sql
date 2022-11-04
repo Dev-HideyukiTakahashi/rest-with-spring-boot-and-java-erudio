e86e2b41e66cce802ac044c00a6068f5461aee4f
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-11-04 03:55:42 -0300

    13_Swagger

---
+++ 13_Swagger/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,72 @@
+INSERT INTO `tb_person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(
+   1,
+   'Postman',
+   'Post',
+   'male',
+   'Test'
+),
+
+(
+   3,
+   'São Paulo',
+   'João',
+   'Male',
+   'Test'
+),
+
+(
+   4,
+   'São Paulo',
+   'Maria',
+   'Female',
+   'Silva Santos'
+),
+
+(
+   5,
+   'Kentucky - US',
+   'Muhammad',
+   'Male',
+   'Ali'
+),
+
+(
+   7,
+   'Mvezo - South Africa',
+   'Nelson',
+   'Male',
+   'Mandela'
+),
+
+(
+   8,
+   'Mvezo - South Africa',
+   'Nelson',
+   'Male',
+   'Mandela'
+),
+
+(
+   9,
+   'Mvezo - South Africa',
+   'Nelson',
+   'Male',
+   'Mandela'
+),
+
+(
+   10,
+   'Smiljan - Croatia',
+   'Nikola',
+   'Male',
+   'Tesla'
+),
+
+(
+   11,
+   'Diadema - Brasil',
+   'Maria',
+   'Female',
+   'Silva'
+);
