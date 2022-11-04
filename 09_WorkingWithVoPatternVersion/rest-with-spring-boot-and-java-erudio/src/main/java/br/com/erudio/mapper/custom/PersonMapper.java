223032837af78f3a6688586ef5ae0c988d8dc5f2
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-11-03 21:07:47 -0300

    09_WorkingWithVoPatternVersion

---
+++ 09_WorkingWithVoPatternVersion/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/mapper/custom/PersonMapper.java
@@ -0,0 +1,36 @@
+package br.com.erudio.mapper.custom;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.data.vo.v2.PersonVOV2;
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonMapper {
+
+	public PersonVOV2 convertEntityToVO(Person person) {
+		PersonVOV2 vo = new PersonVOV2();
+		vo.setId(person.getId());
+		vo.setFirstName(person.getFirstName());
+		vo.setLastName(person.getLastName());
+		vo.setAddress(person.getAddress());
+		vo.setGender(person.getGender());
+		vo.setBirthDay(new Date());
+		
+		return vo;
+	}
+	
+	public Person convertVoToEntity(PersonVOV2 person) {
+		Person entity = new Person();
+		entity.setId(person.getId());
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		//entity.setBirthDay(new Date());
+		
+		return entity;
+	}
+}
