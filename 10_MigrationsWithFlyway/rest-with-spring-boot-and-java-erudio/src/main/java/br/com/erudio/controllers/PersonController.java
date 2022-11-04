42fdc447fe199d0da7c7bf9a6770678fb2d6bf64
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-11-03 23:00:11 -0300

    10_MigrationsWithFlyway

---
+++ 10_MigrationsWithFlyway/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,61 @@
+package br.com.erudio.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.services.PersonServices;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, 
+			        produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception{	
+		
+		return service.findById(id);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> deleteById(@PathVariable(value = "id") Long id) throws Exception{	
+		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO create(@RequestBody PersonVO person) throws Exception{	
+		
+		return service.create(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO update(@RequestBody PersonVO person) throws Exception{	
+		
+		return service.update(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll() {	
+		
+		return service.findAll();
+	}
+}
