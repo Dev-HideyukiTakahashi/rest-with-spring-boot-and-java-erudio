1257be919d22ef08c7ad2947c517fc2a247ea2a1
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-11-04 22:15:30 -0300

    13_Swagger Pt2

---
+++ 13_Swagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/BookVO.java
@@ -0,0 +1,85 @@
+package br.com.erudio.data.vo.v1;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+
+@JsonPropertyOrder({"id", "author", "launchDate", "price", "title"})
+public class BookVO extends RepresentationModel<BookVO> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonProperty(value = "id")
+	@Mapping("id")
+	private Long key;
+	private String author;
+	private Date launchDate;
+	private Double price;
+	private String title;
+
+	public Long getKey() {
+		return key;
+	}
+
+	public void setKey(Long key) {
+		this.key = key;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(Date launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(author, key, launchDate, price, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookVO other = (BookVO) obj;
+		return Objects.equals(author, other.author) && Objects.equals(key, other.key)
+				&& Objects.equals(launchDate, other.launchDate) && Objects.equals(price, other.price)
+				&& Objects.equals(title, other.title);
+	}
+
+}
---
+++ 13_Swagger/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -0,0 +1,7 @@
+package br.com.erudio.configs;
+
+public class TestConfigs {
+	
+	public static final int SERVER_PORT = 8888;
+
+}
